5add3954e49857b9827448286fadb8fc6e5abf71
TanerAlbayrak <taner_albayrak@student.uml.edu>
2017-05-12 10:17:51 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,46 @@
+<div class="content row">
+  <div class="col-lg-12">
+    <header class="clearfix">
+      <section id="branding">
+        <a href="index3.php"><img src="/images/logowestnet.png" alt="Logo for Westnet"></a>
+          </section><!-- branding -->
+
+            <section class="navbar">
+
+
+              <ul class="nav navbar-nav">
+
+                <li class="active"><a href="index3.php">Home</a></li>
+                
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+                    <li><a tabindex="-1" href="artists.php">All Products</a></li>
+                    <li class="divider"></li>
+  					<li><a tabindex="-1" href="artists.php#Barot_Bellingham">Equipment</a></li>
+  					<li><a tabindex="-1" href="artists.php#Gerard_Donahue">Safety and Gloves</a></li>
+				  	<li><a tabindex="-1" href="artists.php#Jonathan_Ferrar">Glassware</a></li>
+				  	<li><a tabindex="-1" href="artists.php#Lorenzo_Garcia">Liquid Handling</a></li>
+				  	<li><a tabindex="-1" href="artists.php#Hillary_Goldwynn">Cell and Tissue Culture</a></li>
+				  	<li><a tabindex="-1" href="artists.php#Hassum_Harrod">Cryogenics</a></li>
+				 	<li><a tabindex="-1" href="artists.php#Jennifer_Jerome">Storage, Racks, and Trays</a></li>
+				 	<li><a tabindex="-1" href="artists.php#LaVonne_LaRue">Blood and Sample Collection</a></li>
+				  	<li><a tabindex="-1" href="artists.php#Riley_Rewington">Paper, film, foil, and Janitorial</a></li>
+				  	<li><a tabindex="-1" href="artists.php#Constance_Smith">Biohazard</a></li>
+				 	<li><a tabindex="-1" href="artists.php#Xhou_Ta">Medical and Surgical</a></li>
+				  	<li><a tabindex="-1" href="artists.php#Richard_Tweed">Genomics</a></li>
+                    <li><a tabindex="-1" href="index3.php">PCR and Molecular Biology</a></li>
+                    </ul>
+                </li>
+                <li><a href="index3.php">About Us</a></li>
+                <li><a href="index3.php">Our Customers</a></li>
+
+                <li><a><input class="form control mr-sm-2" type="text" placeholder="Search">
+            	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button></a></li>
+
+              </ul><!-- nav -->
+            </section><!-- navbar -->
+        </header><!-- header -->
+      </div><!-- column -->
+    </div><!-- content -->
+    
